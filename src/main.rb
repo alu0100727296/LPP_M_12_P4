7a8a30ffa66f396a8c065c123e0cf3df17782f19
Julian Villar <alu0100727296@ull.edu.es>
2014-10-17 16:13:06 +0100

    Definicion de main

--- src/main.rb
+++ src/main.rb
@@ -1,1 +1,18 @@
 require './Complejo'
+
+numberOne = Complejo.new(3.0,2.0)
+numberTwo = Complejo.new(0.0,2.0)
+numberThree = Complejo.new(0.0,0.0)
+
+puts "Numero uno = #{numberOne}" 
+puts "Numero dos = #{numberTwo}" 
+puts "Numero tres = #{numberThree}" 
+
+puts "\nNumero uno + Numero dos = #{numberOne.sum(numberTwo)}"
+puts "Numero uno - Numero dos = #{numberOne.subtract(numberTwo)}"
+puts "Numero uno * Numero dos = #{numberOne.product(numberTwo)}"
+puts "Numero uno / Numero dos = #{numberOne.division(numberTwo)}"
+
+puts "\nCambiando el valor de Numero tres por la suma de numero uno mas numero dos."
+numberThree = numberOne.sum(numberTwo)
+puts "Numero tres = #{numberThree}" 
